e3f771026b59e19eeed4934a95e4e2532bce1303
KedroBoss <dimmak90@gmail.com>
2016-12-11 18:58:38 +0300

    Template For Application

---
+++ Section19_MotionDetection_Application/camera_face_detection.py
@@ -0,0 +1,21 @@
+import cv2, time
+
+video=cv2.VideoCapture(0)#Number is camera
+
+while True:
+    a+=1
+    check, frame = video.read()
+
+    #time.sleep(3)
+
+    gray_img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    cv2.imshow('Img', gray_img)
+
+    #res_img=cv2.resize(video, (int(video.shape[1]/2),int(video.shape[0]/2)))
+    key=cv2.waitKey(1)
+    if key == ord('q'):
+        break
+
+
+video.release()
+cv2.destroyAllWindows()
